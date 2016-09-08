91736582a6674e4cad24be6e234c1e2dab732a7b
Rick Winfrey <rewinfrey@github.com>
2016-09-08 10:08:37 -0500

    Update to make interactive; use Church Encoding

--- app/Main.hs
+++ app/Main.hs
@@ -5,124 +5,45 @@
 
 module Main where
 
-import Control.Monad.Free
+import Control.Monad.Free.Church
+-- import Control.Monad.Free
 import Data.Foldable
 import Lib
+import Control.Monad
 
-data Provision next =
-  Begin next |
-  Install String next |
-  IfNotExists String (Free Provision ()) next |
-  Touch String next |
-  Cd String next |
-  MkDir String Bool next |
-  Echo String next |
-  Continue |
-  Done
-  deriving(Functor)
+data CommandF next =
+    PutLine String next
+  | GetLine (String -> next)
+  deriving (Functor)
 
-begin = liftF $ Begin id
+gtLine :: (MonadFree CommandF m) => m String
+gtLine = wrap (GetLine return)
 
-install what = liftF $ Install what id
+putLine :: (MonadFree CommandF m) => String -> m ()
+putLine message = wrap (PutLine message (return ()))
 
-ifNotExists path what = liftF $ IfNotExists path what id
+run :: F CommandF a -> IO a
+run = iterM (\command -> case command of
+  PutLine message next -> do
+    putStrLn message
+    next
+  GetLine next -> do
+    input <- Prelude.getLine
+    next input
+  )
 
-touch path = liftF $ Touch path id
-
-cd path = liftF $ Cd path id
-
-mkDir path wholeTree = liftF $ MkDir path wholeTree id
-
-echo message = liftF $ Echo message id
-
-continue = liftF $ Continue
-
-done = liftF Done
-
--- class InterpretingContext a where
---   run :: Free Provision () -> a
---
--- instance InterpretingContext String where
---   run (Free (Begin next)) =
---     "#!/usr/bin/env bash\n\n" ++ (run next)
---
---   run (Free (Install what next)) =
---     "apt-get install " ++ what ++ "\n" ++ nextStr
---     where
---       nextStr = run next
---
---   run (Free (IfNotExists path what next)) =
---     "if [ ! -f " ++ path ++ " ]; then\n\t" ++ whatStr
---       ++ "\nfi\n" ++ nextStr
---     where
---       whatStr = run what
---       nextStr = run next
---
---   run (Free (Touch path next)) =
---     "touch " ++ path ++ "\n" ++ (run next)
---
---   run (Free (Cd path next)) =
---     "cd " ++ path ++ "\n" ++ (run next)
---
---   run (Free (MkDir path tree next)) =
---     "mkdir " ++ treeOption ++ " " ++ path ++ "\n" ++ (run next)
---     where
---       treeOption =
---         if tree then "-p" else ""
---
---   run (Free (Echo message next)) =
---     "echo " ++ message ++ "\n" ++ run next
---
---   run (Free Continue) = ""
---
---   run (Free Done) = "exit 0"
-
-run (Free (Begin next)) =
-  "#!/usr/bin/env bash\n\n" ++ (run next)
-
-run (Free (Install what next)) =
-  "apt-get install " ++ what ++ "\n" ++ nextStr
-  where
-    nextStr = run next
-
-run (Free (IfNotExists path what next)) =
-  "if [ ! -f " ++ path ++ " ]; then\n\t" ++ whatStr
-    ++ "\nfi\n" ++ nextStr
-  where
-    whatStr = run what
-    nextStr = run next
-
-run (Free (Touch path next)) =
-  "touch " ++ path ++ "\n" ++ (run next)
-
-run (Free (Cd path next)) =
-  "cd " ++ path ++ "\n" ++ (run next)
-
-run (Free (MkDir path tree next)) =
-  "mkdir " ++ treeOption ++ " " ++ path ++ "\n" ++ (run next)
-  where
-    treeOption =
-      if tree then "-p" else ""
-
-run (Free (Echo message next)) =
-  "echo " ++ message ++ "\n" ++ run next
-
-run (Free Continue) = ""
-
-run (Free Done) = "exit 0"
-
-app :: Free Provision ()
+app :: (MonadFree CommandF m) => m ()
 app = do
-  begin
-  install "postgresql-server"
-  mkDir "/var/run/the-app" True
-  cd "/var/run/the-app"
-  ifNotExists "the-app.log" $ touch "the-app.log" >> continue
-  done
+  putLine "What's your name?"
+  input <- gtLine
+  putLine input
+  putLine "What's your age?"
+  input2 <- gtLine
+  putLine input2
+  putLine "Thank you"
 
-runApp :: Free Provision () -> String
 runApp = run
 
-main :: IO String
+main :: IO ()
 main = do
-  return $ runApp app
+  runApp app
